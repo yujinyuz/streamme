6610d0596b84164ca6abedbe77dda09c1b8c8ed9
Eugene Oliveros <eevoliveros@gmail.com>
2019-10-04 11:34:25 +0800

    Basic CRUD using ReactJS + Redux with json-server

---
+++ README.md
@@ -0,0 +1,5 @@
+# streams
+
+Simple CRUD using ReactJS and Redux
+
+Will be providing a documentation for installation process and how to run
---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamEdit extends Component {
+  componentDidMount() {
+    const streamId = this.props.match.params.id;
+    this.props.fetchStream(streamId);
+  }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading. . .</div>;
+    }
+
+    return (
+      <div>
+        <h3>Edit a stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={this.props.stream}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
